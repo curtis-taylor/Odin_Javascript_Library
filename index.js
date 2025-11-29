20d9a496f8cded630da107af8e84e407711c2e5d
curtis-taylor <curtis.t@gmail.com>
2025-11-28 23:13:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const book_screen = document.querySelector('#page_container');
+
+
+const myLibrary = [];
+
+function Book(title, author, pages, have_book) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+   
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.have_book = have_book;
+    this.id = crypto.randomUUID();
+   
+    this.info = function() {
+        const i = {title: title, author: author,
+            pages: pages, have_book: have_book, id: this.id
+        }
+        return i
+    };
+   
+   
+   
+}
+
+function addBookToLibrary(title, author, pages, have_book) {
+    const book = new Book(title, author, pages, have_book);
+    console.log(book.info())
+    myLibrary.push(book);
+    console.log(myLibrary);
+}
+
+addBookToLibrary('LOTR', 'Tolkien', 500, true);
+
+
+
