5ee38d57b4b812a9f0c403540c36dd0122d7f849
curtis taylor <curtis.t@gmail.com>
2025-12-11 03:17:10 -0500

    Added Mark read button

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ const dialog_ok_button = document.querySelector("#ok");
     });
 
   dialog_ok_button.addEventListener('click', e => {
-        const add_dialog = document.querySelector('#add_dialog');
-        const dialog_title = document.querySelector("#booktitle_text").value;
-        const dialog_author = document.querySelector("#book_author_text")
-        const dialog_page = document.querySelector("#page_count_text")
-        const dialog_coverURL = document.querySelector("#bookcover_url_text")
-
+       // const add_dialog = document.querySelector('#add_dialog');
+        const dialog_title = document.querySelector("#book_title_text").value;
+        const dialog_author = document.querySelector("#book_author_text").value;
+        const dialog_page = document.querySelector("#page_count_text").value;
+        const dialog_coverURL = document.querySelector("#bookcover_url_text").value
         const book_container = document.querySelector('#page_container');
 
 
+        console.log("add button clicked: " + dialog_title);
         book_container.setAttribute('style', 'display: grid; grid-template-columns: repeat(5, 1fr);');
     
 
@@ -37,7 +37,7 @@ const dialog_ok_button = document.querySelector("#ok");
         e.stopImmediatePropagation();
         console.log("add button clicked: " + dialog_title);
 
-       // addBookToLibrary(dialog_title., 'Tolkien', 500, "https://i.cbc.ca/1.4158289.1497366023!/fileImage/httpImage/image.png_gen/derivatives/original_780/book-cover-the-lord-of-the-rings-by-j-r-r-tolkien.png", true);
+        addBookToLibrary(dialog_title, dialog_author, dialog_page, dialog_coverURL);
 
 
        
@@ -45,7 +45,7 @@ const dialog_ok_button = document.querySelector("#ok");
 
     });
 
-function Book(title, author, pages, bookcover_url, have_book) {
+function Book(title, author, pages, bookcover_url) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
@@ -53,23 +53,24 @@ function Book(title, author, pages, bookcover_url, have_book) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.have_book = have_book;
+    // this.have_book = have_book;
     this.id = crypto.randomUUID();
     this.bookcover_url = bookcover_url;
    
+    /*
     this.info = function() {
         const i = {title: title, author: author,
             pages: pages, have_book: have_book, id: this.id
         }
-        return i
-    };
+        return i 
+    }; */
    
    
    
 }
 
-function addBookToLibrary(title, author, pages, bookcover_url, have_book) {
-    const book = new Book(title, author, pages, bookcover_url, have_book);
+function addBookToLibrary(title, author, pages, bookcover_url) {
+    const book = new Book(title, author, pages, bookcover_url);
     //console.log(book.info())
     myLibrary.push(book);
     //console.log(myLibrary);
@@ -134,12 +135,20 @@ function displayBooks(library) {
             let delete_id = "delete_" + String(x);
             delete_button.setAttribute("id", delete_id);
 
+            let read_button = document.createElement("button");
+            read_button.style.cssText = "color: black; background-color: lightblue; height: 30px; margin: 10px;"
+            read_button.textContent = "Mark Read";
+            read_button.setAttribute("class", "read_button");
+            let read_id = "read_" + String(x);
+            read_button.setAttribute("id", read_id);
+
 
             book_div.appendChild(bookcover_div);
             book_div.appendChild(title_div);
             book_div.appendChild(author_div);
             book_div.appendChild(page_count_div);
             book_div.appendChild(delete_button);
+            book_div.appendChild(read_button);
 
             book_container.appendChild(book_div)
             x++;
@@ -173,18 +182,18 @@ function displayBooks(library) {
 
 
 
-addBookToLibrary('LOTR', 'Tolkien', 500, "https://i.cbc.ca/1.4158289.1497366023!/fileImage/httpImage/image.png_gen/derivatives/original_780/book-cover-the-lord-of-the-rings-by-j-r-r-tolkien.png", true);
-addBookToLibrary('LOTR: TWO TOWERS', 'Tolkien', 500, "https://i.cbc.ca/1.4158289.1497366023!/fileImage/httpImage/image.png_gen/derivatives/original_780/book-cover-the-lord-of-the-rings-by-j-r-r-tolkien.png", true);
-addBookToLibrary('ROTK', 'Tolkien', 500, "https://i.cbc.ca/1.4158289.1497366023!/fileImage/httpImage/image.png_gen/derivatives/original_780/book-cover-the-lord-of-the-rings-by-j-r-r-tolkien.png", true);
-addBookToLibrary('Christmas Carol', 'Charles Dickens', 500, "https://www.science.smith.edu/climatelit/wp-content/uploads/sites/97/2024/07/812EQMe0FyL-624x953.jpg", true);
+addBookToLibrary('LOTR', 'Tolkien', 500, "https://i.cbc.ca/1.4158289.1497366023!/fileImage/httpImage/image.png_gen/derivatives/original_780/book-cover-the-lord-of-the-rings-by-j-r-r-tolkien.png");
+addBookToLibrary('LOTR: TWO TOWERS', 'Tolkien', 500, "https://i.cbc.ca/1.4158289.1497366023!/fileImage/httpImage/image.png_gen/derivatives/original_780/book-cover-the-lord-of-the-rings-by-j-r-r-tolkien.png");
+addBookToLibrary('ROTK', 'Tolkien', 500, "https://i.cbc.ca/1.4158289.1497366023!/fileImage/httpImage/image.png_gen/derivatives/original_780/book-cover-the-lord-of-the-rings-by-j-r-r-tolkien.png");
+addBookToLibrary('Christmas Carol', 'Charles Dickens', 500, "https://www.science.smith.edu/climatelit/wp-content/uploads/sites/97/2024/07/812EQMe0FyL-624x953.jpg");
 
-addBookToLibrary('KJV Bible', 'Various Authors', 500, "https://cdn.kobo.com/book-images/d637e34d-2856-4a37-8d93-ea7f2cb5bab6/1200/1200/False/king-james-bible-the-holy-bible-authorized-king-james-version-kjv-old-testament-and-new-testaments.jpg", true);
-addBookToLibrary('C Programming', 'Dennis Ritchie', 500, "https://storage.googleapis.com/paysfer_books/9780131103627.jpg", true);
-addBookToLibrary('Fahrenheit 451', 'Ray Bradbury', 500, "https://i.etsystatic.com/7845523/r/il/095621/432071060/il_fullxfull.432071060_5xcf.jpg", true);
-addBookToLibrary('What Color Is Your Parachute?', 'Richard Nelson Bolles', 500, "https://cdn.kobo.com/book-images/9a247440-f0b4-4d97-945a-d05400c2a465/1200/1200/False/what-color-is-your-parachute-2020.jpg", true);
+addBookToLibrary('KJV Bible', 'Various Authors', 500, "https://cdn.kobo.com/book-images/d637e34d-2856-4a37-8d93-ea7f2cb5bab6/1200/1200/False/king-james-bible-the-holy-bible-authorized-king-james-version-kjv-old-testament-and-new-testaments.jpg");
+addBookToLibrary('C Programming', 'Dennis Ritchie', 500, "https://storage.googleapis.com/paysfer_books/9780131103627.jpg");
+addBookToLibrary('Fahrenheit 451', 'Ray Bradbury', 500, "https://i.etsystatic.com/7845523/r/il/095621/432071060/il_fullxfull.432071060_5xcf.jpg");
+addBookToLibrary('What Color Is Your Parachute?', 'Richard Nelson Bolles', 500, "https://cdn.kobo.com/book-images/9a247440-f0b4-4d97-945a-d05400c2a465/1200/1200/False/what-color-is-your-parachute-2020.jpg");
 
-addBookToLibrary('Romeo & Juliet', 'William Shakespeare', 150, "https://i.pinimg.com/736x/7d/a4/d0/7da4d06680ac41dbdcc0c02977b68de1.jpg", true);
-addBookToLibrary('The Richest Man In Babylon?', 'George S. Clason', 200, "https://deepstash.com/_next/image?url=https:%2F%2Fbooks.google.com%2Fbooks%2Fpublisher%2Fcontent%2Fimages%2Ffrontcover%2Ff1tXEAAAQBAJ%3Ffife%3Dw400&w=3840&q=75", true);
+addBookToLibrary('Romeo & Juliet', 'William Shakespeare', 150, "https://i.pinimg.com/736x/7d/a4/d0/7da4d06680ac41dbdcc0c02977b68de1.jpg");
+addBookToLibrary('The Richest Man In Babylon?', 'George S. Clason', 200, "https://deepstash.com/_next/image?url=https:%2F%2Fbooks.google.com%2Fbooks%2Fpublisher%2Fcontent%2Fimages%2Ffrontcover%2Ff1tXEAAAQBAJ%3Ffife%3Dw400&w=3840&q=75");
 
 console.log("::::::::::: " )
 console.log(myLibrary)
